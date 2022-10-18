4fdb8cfbef529f9489ab2012cb83392e9f6b5de8
sreng sun <rocky.sun@pathmazing.com>
2022-10-18 12:45:54 +0700

    add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,27 @@ import counterReducer from '../features/counter/counterSlice';
 import '../features/user/user.slice';
 import { userReducer } from '../features/user/user.slice';
 
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    auth: userReducer,
+    auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
